e79fd402c3430b8f8cc7b54b70cc0b5b2ac47c1c
Akshay Zade <akshay2000@hotmail.com>
2016-03-20 17:20:24 +0530

    Hopefully cred management that will work

---
+++ Helpers/PasswordVaultExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Credentials;
+
+namespace SharpImgur.Helpers
+{
+    public static class PasswordVaultExtensions
+    {
+        public static bool Contains(this PasswordVault vault, string resource, string userName)
+        {
+            try
+            {
+                vault.Retrieve(resource, userName);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- Helpers/SecretsHelper.cs
+++ Helpers/SecretsHelper.cs
@@ -17,6 +17,9 @@ namespace SharpImgur.Helpers
         private const string userNameKey = "UserName";
         private const string expiryKey = "Expiry";
 
+        private const string accessResource = "AccessToken";
+        private const string refreshResource = "RefreshToken";
+
         public static async Task<JObject> GetConfiguration()
         {
             if (configuration == null)
@@ -33,31 +36,22 @@ namespace SharpImgur.Helpers
         public static async Task<string> GetAccessToken()
         {
             string userName = await GetUserName();
-            const string resource = "AccessToken";
             string token;
-            try
-            {
-                DateTime expiry = DateTime.Parse(SettingsHelper.GetLocalValue<string>(expiryKey, DateTime.MinValue.ToString()));
-                if (expiry == DateTime.MinValue)
-                    throw new Exception("Force execute catch block");
-#if DEBUG
-                expiry = DateTime.MinValue;
-#endif
-                if (expiry > DateTime.Now)
-                {
-                    token = GetVault().Retrieve(resource, userName).Password;
-                }
-                else
-                {
-                    token = await AuthenticationHelper.RefreshAccessToken(await GetRefreshToken());
-                    SettingsHelper.SetLocalValue(expiryKey, (await AuthenticationHelper.GetExpiresAt()).ToString());
-                }
-            }
-            catch
+            DateTime expiry = DateTime.Parse(SettingsHelper.GetLocalValue<string>(expiryKey, DateTime.MinValue.ToString()));
+            if (expiry == DateTime.MinValue)
             {
                 token = await AuthenticationHelper.GetAccessToken();
-                PasswordCredential cred = new PasswordCredential(resource, userName, token);
-                GetVault().Add(cred);
+                UpdateCredentials(token, await AuthenticationHelper.GetRefreshToken());
+                SettingsHelper.SetLocalValue(expiryKey, (await AuthenticationHelper.GetExpiresAt()).ToString());
+            }
+            else if (expiry > DateTime.Now)
+            {
+                token = GetVault().Retrieve(accessResource, userName).Password;
+            }
+            else
+            {
+                token = await AuthenticationHelper.RefreshAccessToken(await GetRefreshToken());
+                UpdateCredentials(token, await AuthenticationHelper.GetRefreshToken());
                 SettingsHelper.SetLocalValue(expiryKey, (await AuthenticationHelper.GetExpiresAt()).ToString());
             }
             return token;
@@ -66,18 +60,17 @@ namespace SharpImgur.Helpers
         public static async Task<string> GetRefreshToken()
         {
             string userName = await GetUserName();
-            const string resource = "RefreshToken";
             string token;
-            try
+            var currentVault = GetVault();
+            if (currentVault.Contains(refreshResource, userName))
             {
-                token = GetVault().Retrieve(resource, userName).Password;
+                token = GetVault().Retrieve(refreshResource, userName).Password;
             }
-            catch
+            else
             {
                 token = await AuthenticationHelper.GetRefreshToken();
-                PasswordCredential cred = new PasswordCredential(resource, userName, token);
-                GetVault().Add(cred);
-            }
+                UpdateCredentials(await AuthenticationHelper.GetAccessToken(), token);
+            }            
             return token;
         }
 
@@ -101,15 +94,29 @@ namespace SharpImgur.Helpers
             return userName;
         }
 
+        private static void UpdateCredentials(string accessToken = null, string refreshToken = null)
+        {
+            if (accessToken != null)
+            {
+                PasswordCredential aCred = new PasswordCredential(accessResource, userName, accessToken);
+                GetVault().Add(aCred);
+            }
+            if (refreshToken != null)
+            {
+                PasswordCredential rCred = new PasswordCredential(refreshResource, userName, refreshToken);
+                GetVault().Add(rCred);
+            }
+        }
+
         public static async Task RefreshSecrets()
         {
             string userName = await AuthenticationHelper.GetUserName();
             SettingsHelper.SetLocalValue(userNameKey, userName);
             string accessToken = await AuthenticationHelper.GetAccessToken();
-            PasswordCredential aCred = new PasswordCredential("AccessToken", userName, accessToken);
+            PasswordCredential aCred = new PasswordCredential(accessResource, userName, accessToken);
             GetVault().Add(aCred);
             string refreshToken = await AuthenticationHelper.GetRefreshToken();
-            PasswordCredential rCred = new PasswordCredential("RefreshToken", userName, refreshToken);
+            PasswordCredential rCred = new PasswordCredential(refreshResource, userName, refreshToken);
             GetVault().Add(rCred);
         }
     }

5391b50ac57eff69412c934e8cb8d105638ffb80
Akshay Zade <akshay2000@hotmail.com>
2016-05-24 23:04:38 +0530

    Added API wrapper for comments

---
+++ APIWrappers/Comments.cs
@@ -0,0 +1,19 @@
+﻿using SharpImgur.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpImgur.APIWrappers
+{
+    public static class Comments
+    {
+        public static async Task Vote(long commentId, string vote)
+        {
+            string url = $"comment/{commentId}/vote/{vote}";
+            var result = await NetworkHelper.ExecutePostRequest(url, new Newtonsoft.Json.Linq.JObject());
+            return;
+        }
+    }
+}
